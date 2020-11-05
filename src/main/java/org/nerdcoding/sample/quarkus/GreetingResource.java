78098986308117bfaf73f383e527c8af5afe0ea7
Tobias Koltsch <tobias@koltsch.de>
2020-11-05 05:51:45 +0100

    Add proper logging configuration
    By default everything should be logged to console
    in DEBUG level. Only when the 'prod' environment
    is set, file logging will be activated.

--- src/main/java/org/nerdcoding/sample/quarkus/GreetingResource.java
+++ src/main/java/org/nerdcoding/sample/quarkus/GreetingResource.java
@@ -1,5 +1,7 @@
 package org.nerdcoding.sample.quarkus;
 
+import org.jboss.logging.Logger;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,9 +10,13 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+    private static final Logger LOG = Logger.getLogger(GreetingResource.class);
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        LOG.debug("/hello endpoint was called");
+
         return "hello";
     }
 }
